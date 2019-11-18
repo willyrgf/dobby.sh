22e42e6c2bd05231a498f0a0a4b7fdeb66777869
Willy Romão <willyr.goncalves@gmail.com>
2019-11-18 19:56:30 +0000

    add a default installation for freebsd

---
+++ freebsd_default_installation.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -x
+
+_set_env_vars() {
+    export \
+        INSTALLER="/usr/sbin/pkg"
+        INSTALLER_INSTALL_PARAM="install -y"
+}
+
+_set_packages() {
+    export \
+        PACKAGES='vim git zabbix4-agent'
+}
+
+_install_packages() {
+    for p in ${PACKAGES}; do
+        ${INSTALLER} ${INSTALLER_INSTALL_PARAM} ${p}
+    done
+}
+
+_main() {
+    _set_env_vars
+    _install_packages
+}
+
+_main $@
